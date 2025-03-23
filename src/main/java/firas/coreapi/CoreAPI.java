c13e6d379ed2426181b2c25a1d68059c7966719c
iForMax <feras.gamer.pro@gmail.com>
2025-03-23 02:55:43 +0200

    Added

--- src/main/java/firas/coreapi/CoreAPI.java
+++ src/main/java/firas/coreapi/CoreAPI.java
@@ -1,7 +1,29 @@
 package firas.coreapi;
 
 
+import firas.coreapi.interfaces.StaffModeable;
+
 public class CoreAPI {
 
+    private static CoreAPI instance;
 
+    public static CoreAPI getInstance() {
+        if (instance == null) {
+            instance = new CoreAPI();
+        }
+        return instance;
+    }
+
+    private StaffModeable staffMode;
+
+    public void setStaffMode(StaffModeable staffMode) {
+        this.staffMode = staffMode;
+    }
+
+    public StaffModeable getStaffMode() {
+        if (staffMode == null) {
+            throw new IllegalStateException("StaffMode has not been initialized!");
+        }
+        return staffMode;
+    }
 }
