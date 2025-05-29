e8d1bab090d48185c4385369233327923e4a525c
iForMax <feras.gamer.pro@gmail.com>
2025-05-29 21:55:37 +0300

    updates

--- src/main/java/firas/coreapi/interfaces/BungeeServerable.java
+++
@@ -1,9 +0,0 @@
-package firas.coreapi.interfaces;
-
-
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-
-public interface BungeeServerable {
-    void sendPlayerToServer(ProxiedPlayer player, String serverName);
-
-}
